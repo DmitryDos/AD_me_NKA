5fd9f764a47dcdf5b543cc4feebfa016da6b1eb1
Xpamoff <2005cramar.05@gmail.com>
2024-03-21 19:47:33 +0300

    Added add functionality

--- scripts/main.js
+++ scripts/main.js
@@ -11,21 +11,36 @@ const onFirstInput = (e) => {
     initTable();
 }
 
+const createFile = (elem) => {
+    let file = document.createElement("div");
+    file.className = "upload__elem";
+    file.innerText = elem.name;
+    return file;
+}
+
 const initTable = () => {
     let field = document.querySelector(".upload__field");
     field.classList.remove("upload__field-disabled");
     let list = document.querySelector(".upload__list");
     for (let elem of window.files) {
-        let file = document.createElement("div");
-        file.className = "upload__elem";
-        file.innerText = elem.name;
-        list.append(file);
+        list.append(createFile(elem));
+    }
+}
+
+const handleInnerInput = () => {
+    let list = document.querySelector(".upload__list");
+    let files = document.querySelector(".upload__button-add__input").files;
+    for (let elem of files) {
+        window.files.push(elem);
+        list.append(createFile(elem));
     }
 }
 
 const init = () => {
     let input = document.querySelector(".upload__input");
+    let add = document.querySelector(".upload__button-add__input");
     input.addEventListener("change", onFirstInput);
+    add.addEventListener("change", handleInnerInput);
 }
 
 
